b84a142f293bc72444d0df3cf91344e09c090df3
RealnieShishki <realnieshishki@icloud.com>
2022-05-09 19:02:53 +0400

    Initial commit

--- test_FManager.py
+++ test_FManager.py
@@ -1,6 +1,7 @@
 import os
 import shutil
 
+
 def filenames():
     result = []
     for item in os.listdir():
@@ -8,26 +9,31 @@ def filenames():
             result.append(item)
     return result
 
+
 def author_info():
     return 'Leonid Orlov'
 
+
 def test_filenames():
     assert filenames() == ['account.py',
-                            'Console File Manager.py',
-                            'main.py',
-                            'test_FManager.py',
-                            'test_python.py',
-                            'victorina.py',
-                            'victorina23.py']
+                           'Console File Manager.py',
+                           'main.py',
+                           'test_FManager.py',
+                           'test_python.py',
+                           'victorina.py',
+                           'victorina23.py']
+
 
 def test_author_info():
     assert author_info() == 'Leonid Orlov'
+
 
 def copy_file_or_directory(name, new_name):
     if os.path.isdir(name):
         shutil.copytree(name, new_name)
     else:
         shutil.copyfile(name, new_name)
+
 
 def test_copyfile():
     if 'victorina23.py' in os.listdir():
