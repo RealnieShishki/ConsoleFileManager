134e448bbb15b94e8600f38ec5566b4ca7768a49
RealnieShishki <realnieshishki@icloud.com>
2022-05-09 20:14:47 +0400

    Initial commit

--- test_python.py
+++ test_python.py
@@ -1,20 +1,15 @@
 import math
 
-
 num = (1,2,3,4,5)
-
 
 def test_filter():
     assert list(filter(lambda x: x > 3, num)) == [4, 5]
 
-
 def test_map():
     assert list(map(lambda x: x * x, num)) == [1, 4, 9, 16, 25]
 
-
 def test_sorted():
     assert list(sorted(num, reverse=True)) == [5, 4, 3, 2, 1]
-
 
 def test_math():
     assert math.pi * 4 ** 2 == 50.26548245743669
